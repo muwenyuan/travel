ef9bd27e76c3897ae992c76ec3181d24b2cf8120
muwenyuan <759966801@qq.com>
2018-07-04 16:52:10 +0800

    no message

--- src/page/city/city.vue
+++ src/page/city/city.vue
@@ -2,7 +2,7 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
   </div>
 </template>
 
@@ -10,6 +10,7 @@
 import CityHeader from './components/header.vue';
 import CitySearch from './components/citySearch.vue';
 import CityList from './components/list.vue';
+import axios from "axios"
 export default {
   name: 'city',
   components:{
@@ -19,8 +20,27 @@ export default {
   },
   data(){
     return {
-
+      hotCities:[],
+      cities:{}
     }
+  },
+  methods:{
+    cityInit(){
+      let that=this;
+      axios.get("/api/city.json")
+      .then(function(res){
+        res=res.data;
+        console.log(res)
+        if(res.ret && res.data){
+          const data = res.data
+          that.cities=data.cities
+          that.hotCities=data.hotCities
+        }
+      })
+    }
+  },
+  mounted(){
+    this.cityInit();
   }
 }
 </script>
--- src/page/city/components/list.vue
+++ src/page/city/components/list.vue
@@ -11,13 +11,16 @@
     <div class="cityList">
       <div class="title">热门城市</div>
       <div class="buttonList">
-        <div class="buttonWrap">
-          <div class="button">北京</div>
+        <div class="buttonWrap" v-for="item of hotCities" :key="item.id">
+          <div class="button">{{item.name}}</div>
         </div>
       </div>
     </div>
-    <div class="cityList">
-      <div class="title">A</div>
+    <div class="cityList" v-for="(item,key) of cities" :key="key">
+      <div class="title">{{key}}</div>
+      <div class="letterBox">
+        <div class="letterList" v-for="itemlist of item" :key="itemlist.id">{{itemlist.name}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +28,10 @@
 <script>
 export default {
   name: 'cityList',
+  props:{
+    cities:Object,
+    hotCities:Array
+  },
   data(){
     return {
 
@@ -39,4 +46,5 @@ export default {
 .buttonList{overflow: hidden;padding: .1rem .6rem .1rem .1rem}
 .buttonWrap{width: 33.33%;float: left;}
 .button{margin: .1rem;padding: .1rem 0;text-align: center;border: .02rem solid #ccc;border-radius: .06rem;}
+.letterList{line-height: .76rem;padding-left: .2rem;border-bottom:1px solid #e6e6e6;}
 </style>
